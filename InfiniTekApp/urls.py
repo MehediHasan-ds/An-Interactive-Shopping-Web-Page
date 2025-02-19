8b1f940401e8664a2ac2463eb77670d9d8847254
MehediHasan-ds <mehedi.ds.engr@gmail.com>
2025-02-20 04:30:46 +0600

    index in root folder

--- InfiniTekApp/urls.py
+++ InfiniTekApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('manage-specifications/', views.manage_specifications, name='manage-specifications'),
-    path('save-order/', views.save_order, name='save-order'),
+    path('', views.index, name='index'),  # Homepage
+    path('checkout/', views.checkout, name='checkout'),  # Checkout page
+    path('manage-specifications/', views.manage_specifications, name='manage-specifications'),  # Manage specifications page
+    path('save-order/', views.save_order, name='save-order'),  # Save order endpoint
 ]
